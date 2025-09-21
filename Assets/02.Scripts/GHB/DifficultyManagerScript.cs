fc037b97e4a67314e3aceaaae246d6cc20d10869
고혁빈 <rhgurqls00@gmail.com>
2025-09-21 12:56:28 +0900

    [Feature] 난이도 관리 매니저 로직 구현
    * 타이머 매니저에서 매 초 현재 시간을 담은 이벤트를 발송
    * 난이도 매니저는 이벤트를 구독, 구간을 지날 때마다 난이도 증가 로직 작동

--- Assets/02.Scripts/GHB/DifficultyManagerScript.cs
+++ Assets/02.Scripts/GHB/DifficultyManagerScript.cs
@@ -1,7 +1,45 @@
+using System.Collections.Generic;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
 public class DifficultyManager : MonoBehaviour
 {
+    [Header("난이도가 증가하는 지점(분 단위)")]
+    [SerializeField] private List<float> difficultyIncreaseMinutes = new List<float>();
+    private List<float> difficultyIncreaseTimes;
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        difficultyIncreaseTimes = new List<float>(difficultyIncreaseMinutes.Count);
+        for (int i = 0; i < difficultyIncreaseMinutes.Count; i++)
+        {
+            difficultyIncreaseTimes.Add(difficultyIncreaseMinutes[i] * 60f);
+        }
+
+    }
+
+    void OnEnable()
+    {
+        TimerManager.OnTimerTick += HandleTimerTick;
+    }
+
+    void OnDisable()
+    {
+        TimerManager.OnTimerTick -= HandleTimerTick;
+    }
+
+    private void HandleTimerTick(float elapsedTime)
+    {
+        if (currentIndex >= difficultyIncreaseTimes.Count) return;
+
+        if (elapsedTime >= difficultyIncreaseTimes[currentIndex])
+        {
+            IncreaseDifficulty();
+            currentIndex++;
+        }
+    }
+
     public void IncreaseDifficulty()
     {
         // 타이머 매니저와 난이도 매니저는 역할이 연결되지만, 구분되어야 한다고 생각하여 스크립트 분리
