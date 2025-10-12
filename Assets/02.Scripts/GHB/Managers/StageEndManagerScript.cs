1c79057da50390d9822ff9b0f3cb8d4857d67d03
고혁빈 <rhgurqls00@gmail.com>
2025-10-12 22:59:13 +0900

    [Feature] 로직 수정

--- Assets/02.Scripts/GHB/Managers/StageEndManagerScript.cs
+++ Assets/02.Scripts/GHB/Managers/StageEndManagerScript.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using System;
+using System.Threading;
 
 public class StageEndManager : MonoBehaviour
 {
+    [SerializeField] TimerManager timerManager;
     // 현재 스테이지 번호
     [SerializeField] private int stageIndex;
     // float로 변경하여 밀리초까지 전달
@@ -10,46 +12,28 @@ public class StageEndManager : MonoBehaviour
 
     [SerializeField] private int rewardRatio;
 
-    private float currentTime;
-    private bool isCleared = false;   // 클리어 여부
-
     void OnEnable()
     {
-        TimerManager.OnTimerTick += CacheTime;
-        TimerManager.OnStageClear += HandleStageClear;
         StageFlowManager.OnStageStateChanged += HandleStageStateChanged;
     }
 
     void OnDisable()
     {
-        TimerManager.OnTimerTick -= CacheTime;
-        TimerManager.OnStageClear -= HandleStageClear;
         StageFlowManager.OnStageStateChanged -= HandleStageStateChanged;
-    }
-
-    private void CacheTime(float elapsedTime)
-    {
-        currentTime = elapsedTime;
-    }
-
-    // 클리어 여부 기록
-    private void HandleStageClear()
-    {
-        isCleared = true;
     }
 
     // 게임 종료 시점에서 UI로 정보 전달
     public void NotifyStageEnd()
     {
         Debug.Log("Stage End Triggered");
-        float survivedTime = currentTime;
+        float survivedTime = timerManager.CurrentTime;
         int reward = CalculateReward(survivedTime);
         SaveReward(reward);
-        if (isCleared)
+        if (timerManager.IsCleared)
         {
             UnlockNextStage();
         }
-        OnStageEnd?.Invoke(isCleared, survivedTime, reward);
+        OnStageEnd?.Invoke(timerManager.IsCleared, survivedTime, reward);
     }
 
     private int CalculateReward(float seconds)
@@ -72,7 +56,6 @@ public class StageEndManager : MonoBehaviour
         PlayerPrefs.Save();
     }
 
-    // StageFlowManager 상태 변화에 따라 타이머 일시정지 / 재개
     private void HandleStageStateChanged(StageFlowManager.StageState state)
     {
         if (state == StageFlowManager.StageState.End)
