fcd4aff13342996a3259e7f4438ece4979c7c4e4
MNSK-00 <mskim345678@gmail.com>
2025-11-07 11:11:14 +0900

    [Feature] 경험치, 골드 드랍시 흩뿌리기 기능 구현

--- Assets/02.Scripts/KimMinSeong/BehaviorControllers/EnemyControllers/EBasicDropController.cs
+++ Assets/02.Scripts/KimMinSeong/BehaviorControllers/EnemyControllers/EBasicDropController.cs
@@ -5,6 +5,8 @@ public class EBasicDropController : MonoBehaviour, IDroppable
 {
     private Enemy owner;
     private Transform dropTransform; // 드랍하고자 하는 위치
+    [SerializeField, Range(1f, 10f)] private float minScatterForce = 1f;  // 흩뿌리는 힘의 최소값
+    [SerializeField, Range(1f, 10f)] private float maxScatterForce = 10f;    // 흩뿌리는 힘의 최대값
     [SerializeField] private List<DropEntity> dropEntities;    // 드랍할 객체들을 저장하는 리스트
 
     public void Initialize(Component owner)
@@ -42,8 +44,26 @@ public class EBasicDropController : MonoBehaviour, IDroppable
                 // 무작위 확률로 드랍
                 float randomValue = UnityEngine.Random.Range(0f, 100f);
                 if (randomValue <= entity.DropRate)
-                    PoolManager.instance.Spawn(entity.DropPrefab, dropTransform.position);
+                {
+                    GameObject droppedObj = PoolManager.instance.Spawn(entity.DropPrefab, dropTransform.position);
+
+                    // 드랍한 오브젝트에 RigidBody2D 컴포넌트가 있다면 흩뿌리게 스폰하도록 설정
+                    if (droppedObj.TryGetComponent<Rigidbody2D>(out var rb))
+                        ScatterObject(rb);
+                }
             }
         }
+    }
+
+    // 드랍한 오브젝트를 랜덤하게 흩뿌리는 함수
+    private void ScatterObject(Rigidbody2D rb)
+    {
+        // 단위원 (반지름이 1인 원) 안에서 랜덤한 벡터를 찾음
+        // 해당 벡터를 정규화하여 방향 벡터를 얻음
+        // 해당 방향으로 랜덤한 힘의 크기를 곱하여 최종 벡터를 구함
+        Vector2 randomForce = Random.insideUnitCircle.normalized * Random.Range(minScatterForce, maxScatterForce);
+
+        // 오브젝트에 위에서 구한 힘만큼의 순간 충격을 적용
+        rb.AddForce(randomForce, ForceMode2D.Impulse);
     }
 }
