af19e2f067b8cccb6164d9cd0fb3d5426aa4b943
고혁빈 <rhgurqls00@gmail.com>
2025-11-12 20:15:49 +0900

    [Feature] 아이템 SO 구조 구현

--- Assets/02.Scripts/GHB/TempItemActionSO.cs
+++ Assets/02.Scripts/GHB/TempItemActionSO.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
+// 아이템을 기획 후 로직을 작성할 때 이 스크립트에 execute 함수를 오버라이드
 [CreateAssetMenu(fileName = "TempItemActionSO", menuName = "Scriptable Objects/TempItemActionSO")]
-public class TempItemActionSO : ScriptableObject, IItemAction
+public class TempItemActionSO : ItemSO
 {
-    public void Execute(Transform player)
+    public override void Execute(Transform player)
     {
-        Debug.Log("실행됨");
-        // 여기에 실제 로직 구현
+        Debug.Log($"{itemName} 실행됨");
+        // 여기에 행동 로직 직접 작성
     }
 }
