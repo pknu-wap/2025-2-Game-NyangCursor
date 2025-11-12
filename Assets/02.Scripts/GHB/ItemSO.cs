8d757f772ae964737dbf12f9be48642214742173
고혁빈 <rhgurqls00@gmail.com>
2025-11-12 20:22:49 +0900

    [Feature] 아이템 등급 변수 작성

--- Assets/02.Scripts/GHB/ItemSO.cs
+++ Assets/02.Scripts/GHB/ItemSO.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
 
+public enum ItemRarity
+{
+    Normal,
+    Rare,
+    Legendery
+}
+
 // 아이템 SO 추상 클래스
 public abstract class ItemSO : ScriptableObject
 {
     [Header("공통 데이터")]
     public string itemName;
     public Sprite itemImage;
+    public ItemRarity itemRarity;
     [TextArea(2, 5)] public string itemDescription;
 
     public abstract void Execute(Transform player);
