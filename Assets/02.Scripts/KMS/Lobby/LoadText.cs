4d81122775bfc738f2b016c067ce9d5a4a0903f4
KANG MINSEONG <min201911@gmail.com>
2025-11-26 17:48:34 +0900

    [Feature] 로딩씬 수정

--- Assets/02.Scripts/KMS/Lobby/LoadText.cs
+++ Assets/02.Scripts/KMS/Lobby/LoadText.cs
@@ -2,22 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class LoadText : MonoBehaviour
 {
-    Text loadtext;
+    TextMeshProUGUI loadtext;
     int rand;
     void Start()
     {
-        loadtext = GetComponent<Text>();
+        loadtext = GetComponent<TextMeshProUGUI>();
         rand = Random.Range(1, 8);
         switch (rand)
         {
             case 1:
-                loadtext.text = "쥐 군단 잡으로 가는중..";
+                loadtext.text = "ufo 쥐 군단 잡으로 가는중..";
                 break;
             case 2:
-                loadtext.text = "e키를 눌러서 커서에 탑승 할 수 있습니다";
+                loadtext.text = "ufo 쥐 군단 잡으로 가는중..";
                 break;
             case 3:
                 loadtext.text = "커서에서 내릴때 구멍을 조심하세요!";
